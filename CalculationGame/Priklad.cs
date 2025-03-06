74e1b6c4ebd7f23516a28af5d5a92f27f4309327
Martin <martinpop06@gmail.com>
2025-03-06 21:26:07 +0100

    Add project files.

---
+++ CalculationGame/Priklad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationGame
+{
+    public class Priklad
+    {
+        private int x;
+        private int y;
+        private int vysledek;
+        public int X { get => x; }
+        public int Y { get => y; }
+        public int Vysledek { get => vysledek; }
+
+        public Priklad(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            this.vysledek = x + y;
+        }
+    }
+}
---
+++ CalculationGame/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace CalculationGame
+{
+    internal class Program
+    {
+        private static ManualResetEvent mre = new ManualResetEvent(false);
+        private static Random rnd = new Random();
+
+        static void Main(string[] args)
+        { 
+            int pocetPrikladu = 5;
+            int score = 0;
+          
+            Console.WriteLine("stiskni cokoliv pro start:");
+            Console.ReadLine();
+
+            for (int i = 0; i < pocetPrikladu; i++)
+            {
+                mre.Reset();
+
+                Priklad priklad = new Priklad(rnd.Next(0, 20) - 10, rnd.Next(0, 20) - 10);
+                bool corect = false;
+
+                Thread thread = new Thread(() => 
+                {
+                    string y = priklad.Y > 0 ? priklad.Y.ToString() : $"({priklad.Y})";
+                    Console.WriteLine($"{priklad.X} + {y}");
+                    string? answer = Console.ReadLine();
+
+                    if(int.TryParse(answer,out int parsedAnswer))
+                    {
+                        if (priklad.Vysledek == parsedAnswer)
+                        {
+                           corect = true;
+                        }
+                    }
+
+                    mre.Set();
+                });
+
+                thread.Start();
+
+                if (!mre.WaitOne(5000))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"čas vypršel ({priklad.Vysledek})");
+                }
+                else
+                {
+                    if (corect)
+                    {
+                        score++;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("spravně!");
+                    }
+                    else
+                    {    
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"špatná odpověď ({priklad.Vysledek})");
+                    }
+                }
+                Console.ResetColor();
+            }
+            Console.WriteLine("skóre: "+score+"/"+pocetPrikladu);
+            Environment.Exit(0); //pokud posledni answer je timeout, thread by cekal na ReadLine()
+        }
+    }
+}
